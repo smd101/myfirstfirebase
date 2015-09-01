44d44a13d0c23f89da563b7afdf04931a9575a4b
smd101 <s.shimada@tlega.co.jp>
2015-09-01 15:41:28 +0900

    first commit.

---
+++ app.js
@@ -0,0 +1,6 @@
+$.getScript('https://cdn.firebase.com/js/client/2.2.1/firebase.js', function() {
+  var URL_HOST = 'https://torrid-torch-2431.firebaseio.com';
+  var ref = new Firebase(URL_HOST);
+});
+
+//console.log(ref);

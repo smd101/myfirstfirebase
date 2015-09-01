ba3c227b2d48e8cc0f51fb5b3cab72d77fe2272c
smd101 <s.shimada@tlega.co.jp>
2015-09-01 16:29:49 +0900

    [modify] 整形.

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-$.getScript('https://cdn.firebase.com/js/client/2.2.1/firebase.js', function() {
-  var URL_HOST = 'https://torrid-torch-2431.firebaseio.com';
-  var ref = new Firebase(URL_HOST);
-});
 
-//console.log(ref);
+function start(myDataRef) {
+  firebase = myDataRef;
+
+  keypress();
+  notifier();
+}
+
+function keypress() {
+  $('#message').keypress(function(e) {
+    if( e.keyCode == 13) {
+      var name = $('#name').val();
+      var text = $('#message').val();
+      firebase.push({name: name, text: text});
+      $('#message').val('');
+    }
+  });
+}
+
+function notifier() {
+  firebase.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    display(message.name, message.text);
+  });
+}
+
+function display(name, text) {
+  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messages'));
+  $('#messages')[0].scrollTop = $('#messages')[0].scrollHeight;
+}
